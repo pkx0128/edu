e3530a24c6158ca0e90a9f974200ea2978643363
pankx <764670547@qq.com>
2018-11-29 21:05:07 +0800

    创建属性获取器处理列表页的管理员状态

--- application/index/model/User.php
+++ application/index/model/User.php
@@ -5,6 +5,12 @@
 
     class User extends Model
     {
-      
+      protected function getStatusAttr($value){
+            $status=[
+                0=>'已停用',
+                1=>'已启用'
+            ];
+            return $status[$value];
+      }
     }
 ?>
