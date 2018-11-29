5ebdc1c9707dc363ccc876c47e2fd66d4684c46a
pankx <764670547@qq.com>
2018-11-29 11:56:27 +0800

    新增tdk公共模板，并在相应页面引入

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,6 +6,8 @@ class Index extends Base
     {
         $this->isLogin();
         $this->assign('title','tp5学习项目');
+        $this->assign('keywords','tp5学习项目');
+        $this->assign('des','tp5学习项目');
         return $this-> fetch();
     }
 }
